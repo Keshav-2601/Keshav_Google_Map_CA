6820326ba276f5086614a9bbde97bd05fbd47310
Keshav-2601 <keshavv857@gmail.com>
2024-03-30 22:54:07 +0000

    git 1 intialising  the map

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function loadMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
+        center: new google.maps.LatLng(53.35, -6.26),
         zoom: 10,
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
